b107aa4a619c7d45e64084a271cdc4dfbbef98c4
Kirill Zhitko <kirillzhitko@gmail.com>
2024-04-06 18:29:38 +0300

    Initial commit

---
+++ app/phone/build.gradle.kts
@@ -0,0 +1,63 @@
+import java.util.Calendar
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+fun getCurrentDate(): String {
+    val calendar: Calendar = Calendar.getInstance()
+        ?: throw IllegalStateException("The calendar cannot be retrieved")
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+    return "$year.$month.$day"
+}
+
+android {
+    namespace = "kanti.curriewer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "kanti.curriewer"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = getCurrentDate()
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui.m3)
+    implementation(libs.compose.ui.tooling.preview)
+    debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.compose.activity)
+}
