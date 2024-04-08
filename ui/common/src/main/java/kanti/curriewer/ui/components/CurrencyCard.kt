43e1e7acca7ad74c97e46ee6dffbbeacefbc5618
Kirill Zhitko <kirillzhitko@gmail.com>
2024-04-08 16:45:06 +0300

    FEAT: добавление onClick события в CurrencyCard

--- ui/common/src/main/java/kanti/curriewer/ui/components/CurrencyCard.kt
+++ ui/common/src/main/java/kanti/curriewer/ui/components/CurrencyCard.kt
@@ -1,5 +1,7 @@
 package kanti.curriewer.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,13 +11,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kanti.curriewer.ui.CurriewerTheme
@@ -26,10 +31,17 @@ import kanti.curriewer.ui.Red
 fun CurrencyCard(
     modifier: Modifier = Modifier,
     data: CurrencyData,
-    contentPadding: PaddingValues = PaddingValues(all = 12.dp)
+    contentPadding: PaddingValues = PaddingValues(all = 12.dp),
+    onClick: () -> Unit = {}
 ) = Surface(
     modifier = modifier
-        .clip(RoundedCornerShape(15.dp)),
+        .clip(RoundedCornerShape(15.dp))
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = rememberRipple(),
+            role = Role.Button,
+            onClick = onClick
+        ),
     color = MaterialTheme.colorScheme.surfaceVariant
 ) {
     Row(
