2d8dfddf090348e6137bc94b4431f32dcde0d1e8
Kirill Zhitko <kirillzhitko@gmail.com>
2024-04-08 19:56:56 +0300

    FEAT: главный экран с валютами

--- feat/currency/src/main/java/kanti/currency/ui/screens/main/MainScreen.kt
+++ feat/currency/src/main/java/kanti/currency/ui/screens/main/MainScreen.kt
@@ -1,7 +1,6 @@
 package kanti.currency.ui.screens.main
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
