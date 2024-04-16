9b23a33584cfb4259cb6e99c356d082743557e91
kirillzhitko <kirillzhitko@gmail.com>
2024-04-16 20:36:11 +0300

    FEAT: расширение удалённых ошибок

--- shared/src/main/java/kanti/curriewer/shared/result/RemoteError.kt
+++ shared/src/main/java/kanti/curriewer/shared/result/RemoteError.kt
@@ -3,3 +3,24 @@ package kanti.curriewer.shared.result
 interface RemoteError : DataError
 
 data object NoConnectionError : RemoteError
+
+data object NotAllowedUseEndpointError : RemoteError
+
+data object NotFoundEndpointError : RemoteError
+
+data object ValidationError : RemoteError
+
+data object LimitBeenReached : RemoteError
+
+data object ServerError : RemoteError
+
+fun remoteErrorFromCode(code: Int): RemoteError? {
+	return when (code) {
+		403 -> NotAllowedUseEndpointError
+		404 -> NotFoundEndpointError
+		422 -> ValidationError
+		429 -> LimitBeenReached
+		500 -> ServerError
+		else -> null
+	}
+}
